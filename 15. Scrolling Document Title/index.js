a70f3a89941cc1d7b77357a067fe75ea5c5e4910
Akshat Aggarwal <77603321+AA-Akshat@users.noreply.github.com>
2021-11-05 20:49:48 +0530

    Add files via upload

---
+++ 15. Scrolling Document Title/index.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    var t = document.title;
+    var i = 1;
+
+    function scrollTitle(){
+
+        var len = document.title.length;
+        if(len==0)
+        {
+            i=0;
+        }
+        document.title = t.substring(i,t.length);
+        i++;
+
+        setTimeout(scrollTitle,100);
+    };
+
+    scrollTitle();
+};
